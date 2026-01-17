39431c5f845fd7145b62a9e1f88411cc2a4d24f3
index electronics <jasmin>
2026-01-17 15:53:17 +0400

    clear a mistake

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://personaltaskmanager-kcbj.vercel.app",
+  origin: "https://personaltaskmanager-kcbj.vercel.app",
   credentials: true
 }));
 
